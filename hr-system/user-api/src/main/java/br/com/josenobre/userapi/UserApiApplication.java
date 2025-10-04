3eb62369b6a0685680244e20d3e9c54897d8e20e
unknown <jnobr3c+github@gmail.com>
2025-10-03 23:16:03 -0300

    :bug: fix: Aula 02 - Iniciando o Projeto

--- hr-system/user-api/src/main/java/br/com/josenobre/userapi/UserApiApplication.java
+++ hr-system/user-api/src/main/java/br/com/josenobre/userapi/UserApiApplication.java
@@ -1,30 +1,13 @@
 package br.com.josenobre.userapi;
 
-
-import br.com.josenobre.userapi.domain.User;
-import br.com.josenobre.userapi.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
 @SpringBootApplication
-public class UserApiApplication implements CommandLineRunner {
+public class UserApiApplication {
 
-    @Autowired
-    private UserRepository userRepository;
+	public static void main(String[] args) {
+		SpringApplication.run(UserApiApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(UserApiApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        userRepository.saveAll(List.of(
-                new User(null, "Valdir Cezar", "valdir@mail.com", "123", 20.0),
-                new User(null, "Valdir Cezar", "valdir@mail.com", "123", 20.0)
-        ));
-    }
 }
--- hr-system/user-api/src/main/java/br/com/josenobre/userapi/services/exceptions/ObjectNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.com.josenobre.userapi.services.exceptions;
-
-public class ObjectNotFoundException extends RuntimeException {
-
-    public ObjectNotFoundException(String message) {
-        super(message);
-    }
-}
